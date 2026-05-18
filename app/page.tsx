9f20deecbc84fc691b15852cd80f4103908c2aa3
brianmichaelu <brianmichaelu@gmail.com>
2026-05-18 20:38:36 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -472,7 +472,7 @@ export default function Home() {
   <div className="overflow-hidden rounded-[2rem] bg-white shadow-xl">
 
     <img
-  src="/uniform.png"
+  src="/uniforms.png"
   alt="Corporate Uniforms"
   className="h-64 w-full object-cover"
 />
